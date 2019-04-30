b3ee920bd8e9d256a985adacafef958decd4a195
Barry Skidmore <bskidmore@alertlogic.com>
2019-04-30 13:33:30 -0500

    First commit

---
+++ test/album-client.spec.ts
@@ -0,0 +1,65 @@
+import { AlbumClient } from '../src/index';
+import { expect, assert } from 'chai';
+import { describe, before } from 'mocha';
+import * as sinon from 'sinon';
+
+const serviceName = 'album';
+
+afterEach(() => {
+  sinon.restore();
+});
+describe('Album Client Test Suite:', () => {
+  describe('when fetching images', () => {
+    let stub: sinon.SinonSpy;
+    beforeEach(() => {
+      stub = sinon.stub(AlbumClient['alClient'], 'fetch');
+    });
+    afterEach(() => {
+      stub.restore();
+    });
+    it('should call fetch() on the ALClient instance', async() => {
+      await AlbumClient.getImages({type: 'aws', product: 'bob'});
+      expect(stub.callCount).to.equal(1);
+    });
+  });
+  describe('when fetching shares', () => {
+    let stub: sinon.SinonSpy;
+    beforeEach(() => {
+      stub = sinon.stub(AlbumClient['alClient'], 'fetch');
+    });
+    afterEach(() => {
+      stub.restore();
+    });
+    it('should call fetch() on the ALClient instance', async() => {
+      await AlbumClient.getShares('1234', {type: 'aws', type_id: '123456789012'});
+      expect(stub.callCount).to.equal(1);
+    });
+  });
+  describe('when sharing', () => {
+    let stub: sinon.SinonSpy;
+    beforeEach(() => {
+      stub = sinon.stub(AlbumClient['alClient'], 'set');
+    });
+    afterEach(() => {
+      stub.restore();
+    });
+    it('should call put() on the ALClient instance', async() => {
+      await AlbumClient.shareImage('aws', '123456789012');
+      expect(stub.callCount).to.equal(1);
+    });
+  });
+  describe('when unsharing', () => {
+    let stub: sinon.SinonSpy;
+    beforeEach(() => {
+      stub = sinon.stub(AlbumClient['alClient'], 'delete');
+    });
+    afterEach(() => {
+      stub.restore();
+    });
+    it('should call delete() on the ALClient instance', async() => {
+      await AlbumClient.unshareImage('aws', '123456789012');
+      expect(stub.callCount).to.equal(1);
+    });
+  });
+    
+});
