407f1b847eff0b4c2c94a0833746361ceeeefc18
1kh4l <jhon.pantoja@correounivalle.edu.co>
2019-09-10 10:22:57 -0500

    Adding additional logic handled in ui against Cargo

--- src/cargo-client.ts
+++ src/cargo-client.ts
@@ -2,6 +2,7 @@
  * Module to deal with available Cargo Public API endpoints
  */
 import { ALClient, APIRequestParams } from '@al/client';
+import date from 'date-and-time';
 
 export interface TableauReportDefinition {
   site_id?: string;
@@ -61,12 +62,12 @@ export interface ListScheduledReportsQueryParams {
 
 export interface CargoReportResponse {
   account_id: string;
-  report: CargoReport;
+  report: CargoReportFormatted;
 }
 
 export interface CargoReportListResponse {
   account_id: string;
-  reports: CargoReport[];
+  reports: CargoReportFormatted[];
 }
 
 export interface CargoReport {
@@ -95,13 +96,63 @@ export interface CargoReport {
 
 export interface CargoScheduledReportResponse {
   account_id: string;
-  scheduled_report: CargoReport;
+  scheduled_report: CargoReportFormatted;
 }
 
 export interface CargoScheduledReportListResponse {
   account_id: string;
   continuation: string;
-  scheduled_reports: CargoReport[];
+  scheduled_reports: CargoScheduledReport[];
+}
+class CargoReportFormatted implements CargoReport {
+  public static import = (cargoReports: CargoReport[]): Array<CargoReportFormatted> => {
+    let result: CargoReportFormatted[] = [];
+    cargoReports.forEach(element => {
+      let entity: CargoReportFormatted = new CargoReportFormatted();
+      // Set the name properly.
+      let name = element.hasOwnProperty('schedule_display_name') ? element.schedule_display_name : '';
+      if (name !== null) {
+          if (name.includes('Weekly')) {
+              name = 'Weekly';
+          } else if (name.includes('Daily')) {
+              name = `${name} to ${name.split(',')[1]}`;
+          } else if (name.includes('Hourly')) {
+              name = 'Hourly';
+          }
+      }
+      entity.schedule_display_name = name;
+      result.push(entity);
+    });
+    return result;
+  }
+}
+
+/* It is used for adding suffix to the formated_create_time in CargoScheduledReport */
+const ordinals: string[] = ['th','st','nd','rd'];
+
+class CargoScheduledReport implements CargoReport {
+  formated_create_time: string;
+  frequency_type: string;
+
+  public static import = (cargoReport: CargoReport): CargoScheduledReport => {
+    let entity   = new CargoScheduledReport();
+    let datePipe = new Date();
+    // Helper - date formatter.
+    const formatTime = (timeStamp:number): string  => {
+      const today         = new Date();
+      const date          = date.format(timeStamp, 'MMMM DD, YYYY');
+      const day           = date.format(timeStamp, 'DD');
+      const monthYear     = date.format(timeStamp, 'MMM, YYYY');
+      const nDay          = parseInt(day,10) % 100;
+      const suffix        = (ordinals[(nDay-20)%10] || ordinals[nDay] || ordinals[0]);
+      const formattedDate = `${day}${suffix} ${monthYear}`;
+      return date.format(today, 'MMM DD, YYYY') === date ? 'Today': formattedDate;
+    };
+    const creationTime          = parseInt(cargoReport.create_time, 10) * 1000;
+    entity.formated_create_time = formatTime(creationTime);
+    entity.frequency_type       = cargoReport.schedule_display_name;
+    return entity;
+  }
 }
 
 class CargoClient {
@@ -125,11 +176,15 @@ class CargoClient {
    * Get report for given account_id and report_id
    */
   async getReport(accountId: string, reportId: string) {
-    const report = await this.alClient.fetch({
+    let report = await this.alClient.fetch({
       service_name: this.serviceName,
       account_id: accountId,
       path: `/report/${reportId}`,
     });
+    const cargoReports: Array<CargoReport> = [];
+    cargoReports.push(report.report);
+    const reportFormatted = CargoReportFormatted.import(cargoReports)[0];
+    report.report = reportFormatted;
     return report as CargoReportResponse;
   }
   /**
@@ -159,12 +214,14 @@ class CargoClient {
    * Get list of reports for given account_id
    */
   async listReports(accountId: string, queryParams: {report_type?: string} = {}) {
-    const reports = await this.alClient.fetch({
+    let reports = await this.alClient.fetch({
       service_name: this.serviceName,
       account_id: accountId,
       path: '/report',
       params: queryParams,
     });
+    const reportsFormatted = CargoReportFormatted.import(reports.reports);
+    reports.reports = reportsFormatted;
     return reports as CargoReportListResponse;
   }
   /**
@@ -210,7 +267,10 @@ class CargoClient {
       account_id: accountId,
       path: `/scheduled_report/${scheduleReportId}`,
     });
-    return report as CargoReport;
+    const cargoReports: Array<CargoReport> = [];
+    cargoReports.push(report);
+    const formattedReport = CargoReportFormatted.import(cargoReports)[0];
+    return formattedReport as CargoReportFormatted;
   }
   /**
    * Retrieve result data of scheduled report run for given account_id, scheduled_report_id and result_id
@@ -249,6 +309,12 @@ class CargoClient {
       requestArgs.params = queryParams;
     }
     const result = await this.alClient.fetch(requestArgs);
+    const scheduledReports: CargoScheduledReport[] = [];
+    // Set up the Scheduled Reports.
+    result.scheduled_reports.forEach( (scheduled_report:CargoReport) => {
+      scheduledReports.push(CargoScheduledReport.import(scheduled_report));
+    });
+    result.scheduled_reports = scheduledReports;
     return result as CargoScheduledReportListResponse;
   }
 }
