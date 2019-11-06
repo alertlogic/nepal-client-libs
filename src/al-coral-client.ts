79249c44bb46e76ff168174f6e55580baef532fe
1kh4l <jhon.pantoja@correounivalle.edu.co>
2019-11-06 10:09:39 -0500

    Adding implementation for remaining aecoral api methods

--- src/al-coral-client.ts
+++ src/al-coral-client.ts
@@ -22,9 +22,45 @@ export interface AlIncidentDefinition {
     description:    string;
 }
 
+export interface AlCreateIncidentsFromCorrelationRequest extends AlCreateCorrelationNotificationOnlyRequest {
+    observation?:    AlIncidentDefinition;
+}
+
+export interface AlCorrelationRule {
+    attacker_field:    string;
+    enabled:           boolean;
+    expression:        string;
+    expression_window: string;
+    name:              string;
+    victim_field:      string;
+}
+
 export interface AlIncidentSpecificationResponse {
     severities:     string[];
-    clasifications: { name:string, value:string }[];
+    clasifications: { name: string, value: string }[];
+}
+
+export interface AlCorrelationValidationResponse {
+    agg_select_params:    string[];
+    artifact_name:        string;
+    attacker_field:       string;
+    elab_search:          { omitted: string };
+    enabled:              boolean;
+    expression:           { omitted: string };
+    expression_window:    string;
+    incident:             AlIncidentDefinition;
+    name:                 string;
+    nonagg_select_params: string[];
+    notification:         { filter: { something: boolean }, name: string };
+    notification_keys:    string[];
+    object_name:          string;
+    object_summary:       string;
+    observation_def:      { omitted: string };
+    original_expression:  string;
+    rta:                  { omitted: string };
+    trigger:              { omitted: string };
+    unique:               string;
+    victim_field:         string;
 }
 
 export class AlCoralClientInstance {
@@ -34,12 +70,12 @@ export class AlCoralClientInstance {
     /**
      *  Create correlation rule - notification only / incidents from correlation
      */
-    async createCorrelationRule(accountId: string, correlationRequest: AlCreateCorrelationNotificationOnlyRequest):Promise<string> {
+    async createCorrelationRule(accountId: string, correlationRequest: AlCreateIncidentsFromCorrelationRequest | AlCreateCorrelationNotificationOnlyRequest):Promise<string> {
         const result = await ALClient.post({
             service_name: this.serviceName,
-            account_id: accountId,
-            path: '/correlations',
-            data: correlationRequest,
+            account_id:   accountId,
+            path:         '/correlations',
+            data:         correlationRequest,
         });
         if ( ! result.hasOwnProperty("correlation_id" ) ) {
             throw new AlResponseValidationError(`Service returned unexpected result; missing 'correlation_id' property.` );
@@ -48,13 +84,79 @@ export class AlCoralClientInstance {
     }
 
     /**
+     *  Delete correlation rule
+     */
+     async removeCorrelationRule(accountId: string, correlationId: string) {
+        const correlation =  await ALClient.delete({
+            service_name: this.serviceName,
+            account_id:   accountId,
+            path:         `/correlations/${correlationId}`
+        });
+        return correlation;
+     }
+
+    /**
+     *  Get correlation rule by ID
+     */
+     async getCorrelationRule(accountId: string, correlationId: string): Promise<AlCorrelationRule> {
+        const correlation = await ALClient.get({
+            service_name: this.serviceName,
+            account_id:   accountId,
+            path:         `/correlations/${correlationId}`,
+        });
+        return correlation;
+     }
+
+    /**
+     *  Get all correlation rules
+     */
+     async getAllCorrelations(accountId: string): Promise<AlCorrelationRule[]> {
+         const correlations = await ALClient.get({
+             service_name: this.serviceName,
+             account_id:   accountId,
+             path:         '/correlations'
+         });
+         return correlations;
+     }
+
+    /**
+     *   Update correlation rule
+     */
+     async updateCorrelationRule(accountId: string, correlationId: string, correlation: AlCreateIncidentsFromCorrelationRequest): Promise<string> {
+        const correlation_res = await ALClient.post({
+            service_name: this.serviceName,
+            account_id:   accountId,
+            path:         `/correlations/${correlationId}`,
+            data:         correlation
+        });
+
+        if (!correlation_res.hasOwnProperty("correlation_id")) {
+            throw new AlResponseValidationError(`Service returned unexpected result; missing 'correlation_id' property.`);
+        }
+        return correlation_res.correlation_id as string;
+     }
+
+    /**
+     *  It tests the validity of an input or in a debugging capacity to see what content aecoral would generate for a given input.
+     */
+    async validateCorrelationPolicy(accountId: string, correlation: AlCreateIncidentsFromCorrelationRequest) {
+        const validation = await ALClient.post({
+            service_name: this.serviceName,
+            account_id:   accountId,
+            path:         '/validations/correlations',
+            data:         correlation
+        });
+        return validation as AlCorrelationValidationResponse;
+    }
+
+    /**
      *  Get possible correlation incident severities and classifications.
      */
     async getIncidentSpecifications(accountId: string) {
         const result = await ALClient.get({
             service_name: this.serviceName,
-            account_id: accountId,
-            path: '/incident_spec',
+            account_id:   accountId,
+            path:         '/incident_spec',
         });
         return result as AlIncidentSpecificationResponse;
     }
--- test/al-coral-client.spec.ts
+++ test/al-coral-client.spec.ts
@@ -32,10 +32,19 @@ describe('AECORAL CLIENT', () => {
             description: "Some *markdown* text maybe..."
         }
     };
+    const correlation_example = {
+        attacker_field: null,
+        enabled: true,
+        expression: "SELECT INTERVAL(time_recv, 300) AS interval, parsed.json.eventName AS event, parsed.json.userIdentity.accountId AS account, parsed.json.awsRegion AS region, COUNT(message) AS count FROM logmsgs GROUP BY interval, event, account, region WHERE time_recv BETWEEN 1451606400 AND 1451609400 AND parsed.rule_id = '808DDACC-8310-4AE9-BFA7-BFB66E18275E' HAVING count >= 5",
+        expression_window: "window",
+        name: "Admin Failed Login 3",
+        victim_field: "account"
+    };
+    const correlations = [correlation_example];
 
     describe('When creating a notification-only correlation rule', () => {
         beforeEach(() => {
-            stub = sinon.stub(ALClient as any, 'axiosRequest').returns( Promise.resolve( { status: 200, data: { correlation_id: "12345678" } } ) );
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({ status: 200, data: { correlation_id: "12345678" }}));
         });
         afterEach(() => {
             stub.restore();
@@ -48,7 +57,7 @@ describe('AECORAL CLIENT', () => {
     });
     describe('When fetching incident specifications', () => {
         beforeEach(() => {
-            stub = sinon.stub(ALClient as any, 'axiosRequest').returns( Promise.resolve( { status: 200, data: { correlation_id: "12345678" } } ) );
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({ status: 200, data: { correlation_id: "12345678" }}));
         });
         afterEach(() => {
             stub.restore();
@@ -57,6 +66,58 @@ describe('AECORAL CLIENT', () => {
             await AlCoralClient.getIncidentSpecifications('2');
             expect(stub.callCount).to.equal(1);
             expect(stub.args[0][0].url).to.equal("https://api.cloudinsight.alertlogic.com/aecoral/v1/2/incident_spec");
+        });
+    });
+    describe('When removing a correlation rule', () => {
+        beforeEach(() => {
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({status: 200, data: {}}));
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+        it('Should call the ALClient instance\'s DELETE.', async () => {
+            await AlCoralClient.removeCorrelationRule('2', '12345678');
+            expect(stub.callCount).to.equal(1);
+            expect(stub.args[0][0].url).to.equal("https://api.cloudinsight.alertlogic.com/aecoral/v1/2/correlations/12345678");
+        });
+    });
+    describe('When fetching a particular correlation rule', () => {
+        beforeEach(() => {
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({status: 200, data: correlation_example}));
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+        it('Should call the ALClient instance\'s GET.', async () => {
+            await AlCoralClient.getCorrelationRule('2', '12345678');
+            expect(stub.callCount).to.equal(1);
+            expect(stub.args[0][0].url).to.equal("https://api.cloudinsight.alertlogic.com/aecoral/v1/2/correlations/12345678");
+        });
+    });
+    describe('When fetching all correlation rules', () => {
+        beforeEach(() => {
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({status: 200, data: correlations}));
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+        it('Should call the ALClient instance\'s GET.', async () => {
+            await AlCoralClient.getAllCorrelations('2');
+            expect(stub.callCount).to.equal(1);
+            expect(stub.args[0][0].url).to.equal("https://api.cloudinsight.alertlogic.com/aecoral/v1/2/correlations");
+        });
+    });
+    describe('When updating a correlation rule', () => {
+        beforeEach(() => {
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({status: 200, data: {correlation_id: "12345678"}}));
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+        it('Should call the ALClient instance\'s PUT.', async () => {
+            await AlCoralClient.updateCorrelationRule('2', '12345678', correlation_example);
+            expect(stub.callCount).to.equal(1);
+            expect(stub.args[0][0].url).to.equal("https://api.cloudinsight.alertlogic.com/aecoral/v1/2/correlations/12345678");
         });
     });
 });
