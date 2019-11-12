20fafd591cbc124c498b1e2c4051fa9aaea8e7d1
Gisler Garces <ggarces@alertlogic.com>
2019-11-12 11:31:58 -0500

    Adding a first unit test and a first api call

--- src/al-herald-client.ts
+++ src/al-herald-client.ts
@@ -4,7 +4,31 @@
 import { AlResponseValidationError } from '@al/common';
 import { ALClient } from '@al/client';
 
+
+export interface ALSubscriptionKey
+{
+    feature:    string;
+    subkey:     string;
+    name:       string;
+    created:    { at: string, by: string};
+    modified:   { at: string, by: string};
+}
+
 export class AlHeraldClientInstance {
 
     private serviceName = 'herald';
+    private serviceVersion = 'v1';
+
+    /**
+     *  Get all subscriptions keys
+     */
+    async getAllSubscriptionKeys(): Promise<ALSubscriptionKey[]> {
+        const subscriptionKeys = await ALClient.get({
+            service_name: this.serviceName,
+            version:      this.serviceVersion,
+            path:         '/subscription_keys'
+        });
+        return subscriptionKeys;
+    }
+
 }
--- test/al-herald-client.spec.ts
+++ test/al-herald-client.spec.ts
@@ -16,9 +16,48 @@ afterEach(() => {
 let stub: sinon.SinonSpy;
 
 describe('HERALD CLIENT', () => {
-    describe('When you have to write a first working test to validate setup', () => {
-        it('Should run unit test', async () => {
-            expect(1).to.equal(1);
+    const subscriptionkeysMock = {
+        "subscription_keys":[
+           {
+              "subkey":"feature/primary",
+              "name":"Primary Feature",
+              "modified":{
+                 "by":"EA0F10DF-572B-40D6-B467-6C0AA86741DD",
+                 "at":1559834538
+              },
+              "feature":"feature",
+              "created":{
+                 "by":"8C8177B4-4542-427C-A4D1-0EE4672A223F",
+                 "at":1528221409
+              }
+           },
+           {
+              "subkey":"feature2/secondary",
+              "name":"Secondary Feature 2",
+              "modified":{
+                 "by":"EA9F00DF-573B-40D6-B567-6C0AA87645DD",
+                 "at":1559834539
+              },
+              "feature":"feature2",
+              "created":{
+                 "by":"8C9177B4-4542-427C-A4D1-1EE4672A307F",
+                 "at":1528221453
+              }
+           }
+        ]
+    };
+
+    describe('When fetching all subscription keys', () => {
+        beforeEach(() => {
+            stub = sinon.stub(ALClient as any, 'axiosRequest').returns(Promise.resolve({status: 200, data: subscriptionkeysMock}));
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+        it('Should call the AlHeraldClient instance\'s GET.', async () => {
+            await AlHeraldClient.getAllSubscriptionKeys();
+            expect(stub.callCount).to.equal(1);
+            expect(stub.args[0][0].url).to.equal("https://api.cloudinsight.alertlogic.com/herald/v1/subscription_keys");
         });
     });
 });
