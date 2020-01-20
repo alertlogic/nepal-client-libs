3ca0547c40f8c645457ecb12693ca1143dee4d74
Maryit <msanchez@alertlogic.com>
2020-01-20 10:39:55 -0500

    exporting types

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { AlGlobalizer } from '@al/common';
 import { AlHeraldClientInstance } from './al-herald-client';
 import { AlHeraldClientInstanceV2 } from './al-herald-client-v2';
 
-export * from './al-herald-client';
-export * from './types';
 /* tslint:disable:variable-name */
 export const AlHeraldClient = AlGlobalizer.instantiate( "AlHeraldClient", () => new AlHeraldClientInstance() );
 export const AlHeraldClientV2 = AlGlobalizer.instantiate( "AlHeraldClientV2", () => new AlHeraldClientInstanceV2() );
+/* tslint:enable:variable-name */
+
+export * from './al-herald-client';
+export * from './al-herald-client-v2';
+export * from './types';
 
