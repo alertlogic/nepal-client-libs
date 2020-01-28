d6260e9a8f43ab4752ac3f1ca42ec2df2c9d2064
Juan Camilo Kremer <jkremer@alertlogic.com>
2020-01-28 14:33:39 -0500

    US113049: First version of types

--- src/al-applications-client.ts
+++ src/al-applications-client.ts
@@ -2,6 +2,7 @@
  * Applications API client
  */
 import { AlApiClient, AlDefaultClient, AIMSAccount } from '@al/client';
+import { AlApplication } from './types';
 
 export class AlApplicationsClientInstance {
 
@@ -11,5 +12,26 @@ export class AlApplicationsClientInstance {
 
     constructor(client:AlApiClient = null) {
         this.client = client || AlDefaultClient;
+    }
+
+    /**
+     * Return the catalog of application definitions with their default log collection configuration
+     * GET
+     * /applications/v1/:account_id/applications
+     * "https://api.product.dev.alertlogic.com/applications/v1/01000001/applications"
+     *
+     *  @param accountId AIMS Account ID
+     *  @returns a promise with the applications list
+     *
+     *  @remarks
+     *  https://console.product.dev.alertlogic.com/api/applications/#api-Applications-ListApplications
+     */
+    async getAllApplication(accountId: string): Promise<AlApplication[]> {
+        const applicationList = await this.client.get({
+            service_name: this.serviceName,
+            version: this.serviceVersion,
+            account_id: accountId
+        });
+        return applicationList as AlApplication[];
     }
 }
