27b18fbd24f7671b2b912c23da4ab9e378b93c07
Fair Tarapues <fair.tarapues@alertlogic.com>
2020-03-16 10:10:11 -0500

    Adding Time Format

--- src/types/index.ts
+++ src/types/index.ts
@@ -15,6 +15,7 @@ export interface AlApplicationMessageSplitSpec {
 
 export interface AlApplicationMessageTimestamp {
     type?: "automatic" | "datetime";
+    format?: string;
 }
 
 export interface AlApplicationAutomaticFilename {
