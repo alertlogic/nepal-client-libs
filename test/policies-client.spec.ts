b074175db1ef8a3d892bc79d8f27f79cf0012cac
Robert Parker <robert.parker@alertlogic.com>
2020-04-14 15:48:10 +0100

    add tests

---
+++ test/policies-client.spec.ts
@@ -0,0 +1,41 @@
+import { AlPoliciesClientInstance } from '../src/index';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import * as sinon from 'sinon';
+
+let policiesClient:AlPoliciesClientInstance;
+
+beforeEach(() => {
+  policiesClient = new AlPoliciesClientInstance();
+});
+afterEach(() => {
+  sinon.restore();
+});
+describe('Policies Client Test Suite:', () => {
+  describe('when fetching a single policy', () => {
+    let stub: sinon.SinonSpy;
+    beforeEach(() => {
+      stub = sinon.stub(policiesClient['client'], 'get');
+    });
+    afterEach(() => {
+      stub.restore();
+    });
+    it('should call fetch() on the ALClient instance', async () => {
+      await policiesClient.getPolicy('1234', '0987');
+      expect(stub.callCount).to.equal(1);
+    });
+  });
+  describe('when listing policies', () => {
+    let stub: sinon.SinonSpy;
+    beforeEach(() => {
+      stub = sinon.stub(policiesClient['client'], 'get');
+    });
+    afterEach(() => {
+      stub.restore();
+    });
+    it('should call fetch() on the ALClient instance', async () => {
+      await policiesClient.listPolicies('1234');
+      expect(stub.callCount).to.equal(1);
+    });
+  });
+});
