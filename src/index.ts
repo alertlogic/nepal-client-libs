654cb061b6647b01d555616d9bc13d61b0c31df2
scottarver <scottarver@gmail.com>
2020-04-14 09:58:40 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { AlGlobalizer } from '@al/common';
 import { AlPoliciesClientInstance } from './al-policies-client';
 
 /* tslint:disable:variable-name */
-export const AlPoliciesClient = AlGlobalizer.instantiate( "al.policies", () => new AlPoliciesClientInstance() );
+export const AlPoliciesClient = AlGlobalizer.instantiate('al.policies', () => new AlPoliciesClientInstance());
 
 export { AlPoliciesClientInstance } from './al-policies-client';
