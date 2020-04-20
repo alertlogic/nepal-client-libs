46c80ec961b9ffc1266be1e121c150dc762133c1
harold-velez <harold.velez@alertlogic.com>
2020-04-20 15:39:53 -0500

    Adding code coverage report part II

--- packages/otis-client/karma.conf.js
+++ packages/otis-client/karma.conf.js
@@ -32,10 +32,15 @@ module.exports = function (config) {
       reports:
       {
         "html": {
-          "directory": "coverage",
+          "directory": "./../../coverage/otis-client",
           "subdirectory": "report"
         },
-        "text-summary": ""
+        "text-summary": "",
+        "json-summary": {
+          "directory": "./../../coverage/otis-client",
+          "subdirectory": "summary",
+          "filename": "json-summary.json"
+        }
       },
     },
 
