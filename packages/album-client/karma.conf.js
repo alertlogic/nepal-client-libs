2f42ab11b0b77bcaf01209f7357fa0e82597e811
Jason Tarver <jtarver@alertlogic.com>
2020-04-20 17:13:30 -0500

    bringing in new clients

--- packages/album-client/karma.conf.js
+++ packages/album-client/karma.conf.js
@@ -1,44 +1,44 @@
 module.exports = function (config) {
-    config.set({
+  config.set({
 
-        frameworks: ["mocha", "karma-typescript"],
+    frameworks: ["mocha", "karma-typescript"],
 
-        files: [
-            { pattern: "src/**/*.ts" },
-            { pattern: "test/**/*.ts" }
-        ],
+    files: [
+      { pattern: "src/**/*.ts" },
+      { pattern: "test/**/*.ts" }
+    ],
 
-        preprocessors: {
-            "**/*.ts": ["karma-typescript"]
+    preprocessors: {
+      "**/*.ts": ["karma-typescript"]
+    },
+
+    reporters: ["dots", "karma-typescript"],
+
+    browsers       : ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base : 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
+
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        acornOptions: {
+          ecmaVersion: 8,
         },
-
-        reporters: ["dots", "karma-typescript"],
-
-        browsers       : ['ChromeHeadlessNoSandbox'],
-        customLaunchers: {
-            ChromeHeadlessNoSandbox: {
-                base : 'ChromeHeadless',
-                flags: ['--no-sandbox'],
-            },
+      },
+      tsconfig: "tsconfig.spec.json",
+      reports:
+      {
+        "html": {
+          "directory": "coverage",
+          "subdirectory": "report"
         },
+        "text-summary": ""
+      },
+    },
 
-        karmaTypescriptConfig: {
-            bundlerOptions: {
-                acornOptions: {
-                    ecmaVersion: 8,
-                },
-            },
-            tsconfig: "tsconfig.spec.json",
-            reports:
-                {
-                    "html": {
-                        "directory": "coverage",
-                        "subdirectory": "report"
-                    },
-                    "text-summary": ""
-                },
-        },
-
-        singleRun: true
-    });
+    singleRun: true
+  });
 };
