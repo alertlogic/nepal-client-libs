d2653880e45d4bd55acc0d361ea0d20b53b3fae7
scottarver <jtarver@alertlogic.com>
2020-04-24 12:25:42 -0500

    US0 - vr tests in one place (#13)
    * US0 - vr tests in one place
    
    * US0 - vr tests in one place

---
+++ packages/vr-test-helper/src/element-helpers.ts
@@ -0,0 +1,104 @@
+/**
+ * ElementHelpers
+ * Class that provides utilities for e2e tests
+ */
+import * as fs from 'fs';
+import * as path from 'path';
+import {
+    browser,
+    ElementFinder,
+    ExpectedConditions,
+    promise,
+} from 'protractor';
+
+export const WAIT_MAX_TIME = 35000;
+
+interface Coordinates {
+    x: number;
+    y: number;
+}
+
+function ensureDirectoryExistence(filePath: string) {
+    const dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
+
+
+/**
+ * Verify if an element has an specific class
+ * @param element
+ * @param cls
+ */
+export function hasClass(element, cls) {
+    return element.getAttribute('class').then((classes) => classes.split(' ').indexOf(cls) !== -1);
+}
+
+export function writeScreenShot(data, filename) {
+    console.log('writing file to ' + filename);
+    ensureDirectoryExistence(filename);
+    fs.writeFileSync(filename, data, { encoding: 'base64' });
+    return filename;
+}
+
+export function takeScreenshot(name: string) {
+    return browser.takeScreenshot().then((png) => writeScreenShot(png, `${name}-${new Date().getTime()}.png`));
+}
+
+/**
+ * Wait until an element has a class Name
+ * @param element
+ * @param className
+ */
+export function waitForClass(element, className) {
+    browser.wait(
+        () => hasClass(element, className),
+        4000,
+        `Not able to find ${className}`,
+    );
+}
+
+/**
+ * Wait for the presence of an element
+ * @param element
+ */
+export function waitForElementPresence(element: ElementFinder) {
+    const until = ExpectedConditions;
+    browser.wait(
+        until.presenceOf(element),
+        WAIT_MAX_TIME,
+        `Not able to find element, taking more than ${WAIT_MAX_TIME}ms`,
+    );
+}
+
+/**
+ * @param element
+ * @return an object that contains element Coordinates (x,y)
+ */
+export async function getCoordinates(element: ElementFinder): Promise<Coordinates> {
+    const elementPosition = await element.getLocation();
+    const coordinates = {
+        x: elementPosition.x,
+        y: elementPosition.y,
+    };
+    return coordinates;
+}
+
+/**
+ * Scrolls to specific 2D position (x,y)
+ * @param x
+ * @param y
+ * @return a promise of a script that scrolls to specific coordinates
+ */
+export function scrollTo(coordinates: Coordinates): promise.Promise<any> {
+    const script = `window.scrollTo(${coordinates.x}, ${coordinates.y});`;
+    return browser.executeScript(script);
+}
+
+export function scrollToElement(scrollToElement): Promise<unknown> {
+    const wd = browser.driver;
+    return scrollToElement.getLocation().then(loc => wd.executeScript('window.scrollTo(0,arguments[0]);', loc.y));
+}
