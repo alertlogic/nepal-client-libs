02fb29b18a65528ae1c6d295b4fd7d472a89e305
Jason Tarver <jtarver@alertlogic.com>
2020-05-01 13:15:10 -0500

    scrolly scrolly down the page and take pictures

---
+++ packages/vr-test-helper/src/scroll-the-page.ts
@@ -0,0 +1,37 @@
+/**
+ * @author Jason Tarver <scottarver@gmail.com
+ * @date 5/1/20 1:02 PM
+ */
+
+import { browser } from 'protractor';
+
+
+export async function scrollThePage(
+    pictureName: string,
+    pictureFunction: (name: string) => Promise<unknown>,
+    maxScrolls = 25,
+): Promise<void> {
+
+
+    let scrollHeight = parseInt(await browser.executeScript('return document.body.scrollHeight'), 10);
+    const innerHeight = parseInt(await browser.executeScript('return window.innerHeight'), 10);
+
+
+    // console.log(scrollHeight);
+    // console.log(innerHeight);
+    console.log(`${pictureName} will need this many screenshots:${Math.ceil(scrollHeight / innerHeight)}`);
+
+    let counter = 0;
+    let i = 0;
+    while (counter <= scrollHeight && i < maxScrolls) {
+        const thisMuch = Math.floor(i * innerHeight * .9);
+        counter += thisMuch;
+
+        await browser.executeScript(`window.scrollTo(0,${thisMuch})`);
+        await browser.sleep(300);
+        await pictureFunction(`${pictureName}_scrolledDown_${i}_${thisMuch}`);
+
+        scrollHeight = await browser.executeScript('return document.body.scrollHeight');
+        i++;
+    }
+}
