f018860eeedd09e36f13e2d71585d888b73d9989
Jorge Mario Valencia <vamajor@gmail.com>
2020-05-18 19:34:00 -0500

    Adding types from API docs (Swagger openAPI rocks)

--- packages/nepal-scan-scheduler/src/al-scan-scheduler-client.v2.ts
+++ packages/nepal-scan-scheduler/src/al-scan-scheduler-client.v2.ts
@@ -6,53 +6,33 @@ import {
   AlResponseValidationError,
 } from '@al/core';
 
-export interface AlScanScheduleV2 {
-    type: string;
-    name: string;
-    active: boolean;
-    default: boolean;
-}
-
-export interface AlScanScheduleResponseV2 extends AlScanScheduleV2 {
-    created?: {
-        at: number,
-        by: string
-    } | null;
-    modified?: {
-        at: number,
-        by: string
-    } | null;
-    triggered?:  {
-        at: number,
-        by: string
-    } | null;
-}
-
-export interface AlScheduleSummaryResponseV2 {
-    last_scan_time: number;
-    total_assets: number;
-}
-
-export interface AlScanSchedulesResponseV2 {
-    schedules: AlScanScheduleResponseV2[] | {[key: string]: AlScanScheduleResponseV2[]}[];
-    stats?: Stats;
-    total: number;
-}
-
-export interface Stats {
-    [key: string]: {
-        [key: string]: number;
-    };
-}
+import {
+    Schedule,
+    ScheduleSummary
+} from './types/models';
 
 export class AlScanSchedulerClientInstanceV2 {
 
     private serviceName = 'scheduler';
 
     /**
-     *  Create scan schedule
+     *  Lists scan schedules that belong to a particular deployment
      */
-    async createScanSchedule(accountId: string, deploymentId: string, schedule: AlScanScheduleV2): Promise<AlScanScheduleResponseV2> {
+    async getScanSchedulesList(accountId: string, deploymentId: string, params = {}): Promise<Array<Schedule>> {
+         const schedules = await AlDefaultClient.get({
+             service_name: this.serviceName,
+             version: 2,
+             account_id:   accountId,
+             path:         `/${deploymentId}/schedules`,
+             params:       params
+         });
+         return schedules;
+    }
+
+    /**
+     *  Create new Scan Schedule for a given deployment
+     */
+    async createScanSchedule(accountId: string, deploymentId: string, schedule: Schedule): Promise<Schedule> {
         const result = await AlDefaultClient.post({
             service_name: this.serviceName,
             version: 2,
@@ -63,11 +43,24 @@ export class AlScanSchedulerClientInstanceV2 {
         if ( ! result.hasOwnProperty("id" ) ) {
             throw new AlResponseValidationError(`Service returned unexpected result; missing 'id' property.` );
         }
-        return result as AlScanScheduleResponseV2;
+        return result;
     }
 
     /**
-     *  Delete scan schedule
+     *  Returns the scan schedule definition for a given id
+     */
+    async getScanSchedule(accountId: string, deploymentId: string, scheduleId: string): Promise<Schedule> {
+        const schedule = await AlDefaultClient.get({
+            service_name: this.serviceName,
+            version: 2,
+            account_id:   accountId,
+            path:         `/${deploymentId}/schedules/${scheduleId}`,
+        });
+        return schedule;
+    }
+
+    /**
+     *  Deletes Selected Schedule
      */
     async removeScanSchedule(accountId: string, deploymentId: string, scheduleId: string) {
         const response =  await AlDefaultClient.delete({
@@ -80,60 +73,33 @@ export class AlScanSchedulerClientInstanceV2 {
     }
 
     /**
-     *  Get scan schedule by ID
+     *   Updates a selected scan schedule
      */
-    async getScanSchedule(accountId: string, deploymentId: string, scheduleId: string): Promise<AlScanScheduleResponseV2> {
-        const schedule = await AlDefaultClient.get({
-            service_name: this.serviceName,
-            version: 2,
-            account_id:   accountId,
-            path:         `/${deploymentId}/schedules/${scheduleId}`,
-        });
-        return schedule as AlScanScheduleResponseV2;
-    }
-
-    /**
-     *  Get scan schedule summary by ID
-     */
-    async getScanScheduleSummary(accountId: string, deploymentId: string, scheduleId: string): Promise<AlScheduleSummaryResponseV2> {
-        const schedule = await AlDefaultClient.get({
-            service_name: this.serviceName,
-            version: 2,
-            account_id:   accountId,
-            path:         `/${deploymentId}/schedules/${scheduleId}/summary`,
-        });
-        return schedule as AlScheduleSummaryResponseV2;
-    }
-
-    /**
-     *  Get list of scan schedules
-     */
-    async getAllScanSchedules(accountId: string, deploymentId: string, params = {}): Promise<AlScanSchedulesResponseV2> {
-         const schedules = await AlDefaultClient.get({
-             service_name: this.serviceName,
-             version: 2,
-             account_id:   accountId,
-             path:         `/${deploymentId}/schedules`,
-             params:       params
-         });
-         return schedules as AlScanSchedulesResponseV2;
-    }
-
-    /**
-     *   Update schedule
-     */
-    async updateScanSchedule(accountId: string, deploymentId: string, scheduleId: string, schedule: AlScanScheduleV2): Promise<AlScanScheduleResponseV2> {
+    async updateScanSchedule(accountId: string, deploymentId: string, scheduleId: string, schedule: Schedule): Promise<Schedule> {
         const result = await AlDefaultClient.put({
             service_name: this.serviceName,
             version: 2,
             account_id:   accountId,
-            path:         `/schedules/${scheduleId}`,
+            path:         `/${deploymentId}/schedules/${scheduleId}`,
             data:         schedule
         });
 
         if (!result.hasOwnProperty("id")) {
             throw new AlResponseValidationError(`Service returned unexpected result; missing 'id' property.`);
         }
-        return result as AlScanScheduleResponseV2;
+        return result;
+    }
+
+    /**
+     *  Get scan schedule summary by ID
+     */
+    async getScanScheduleSummary(accountId: string, deploymentId: string, scheduleId: string): Promise<ScheduleSummary> {
+        const scheduleSummary = await AlDefaultClient.get({
+            service_name: this.serviceName,
+            version: 2,
+            account_id:   accountId,
+            path:         `/${deploymentId}/schedules/${scheduleId}/summary`,
+        });
+        return scheduleSummary;
     }
 }
--- packages/nepal-scan-scheduler/src/index.ts
+++ packages/nepal-scan-scheduler/src/index.ts
@@ -2,5 +2,6 @@ import { AlGlobalizer } from '@al/core';
 import { AlScanSchedulerClientInstanceV2 } from './al-scan-scheduler-client.v2';
 
 export * from './al-scan-scheduler-client.v2';
+export * from './types/models';
 /* tslint:disable:variable-name */
 export const AlScanSchedulerClient = AlGlobalizer.instantiate( "AlScanSchedulerClient", () => new AlScanSchedulerClientInstanceV2() );
