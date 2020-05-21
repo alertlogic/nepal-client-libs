6c7ed6bef97b4c143e257f81ebe57a2fd878ebcc
Maryit <msanchez@alertlogic.com>
2020-05-21 14:18:28 -0500

    US000: moving themis client

--- packages/themis-client/src/themis-client.ts
+++ packages/themis-client/src/themis-client.ts
@@ -1,7 +1,10 @@
 /**
  * Module to deal with available Themis Public API endpoints
  */
-import { ALClient } from '@al/client';
+import {
+  AlApiClient,
+  AlDefaultClient,
+} from '@al/core';
 
 export interface ThemisRoleDocument {
   type?: 'ci_full' | 'cd_full' | 'ci_x_account_ct' | 'ci_readonly' | 'ci_essentials';
@@ -11,10 +14,10 @@ export interface ThemisRoleDocument {
   aws_account_id?: string;
   version?: string;
   cft?: {
-    s3_bucket?: string;
-    s3_key?: string;
-    s3_url?: string;
-  }
+      s3_bucket?: string;
+      s3_key?: string;
+      s3_url?: string;
+  };
 }
 
 export interface AWSRole {
@@ -33,39 +36,42 @@ export interface AWSRoleValidationResponse {
 
 class ThemisClient {
 
-  private alClient = ALClient;
   private serviceName = 'themis';
+
+  constructor(
+      public client:AlApiClient = AlDefaultClient
+  ) {}
 
   async getRole(
       accountId: string,
-      role_type: 'ci_full' | 'cd_full' | 'ci_x_account_ct' | 'ci_readonly' | 'ci_essentials',
-      platform_type: 'aws' | 'azure'
-    ) {
-    const role = await this.alClient.fetch({
-      service_name: this.serviceName,
-      account_id: accountId,
-      path: `/roles/${platform_type}/${role_type}/latest`,
-    });
-    return role as ThemisRoleDocument;
+      roleType: 'ci_full' | 'cd_full' | 'ci_x_account_ct' | 'ci_readonly' | 'ci_essentials',
+      platformType: 'aws' | 'azure'
+  ) {
+      const role = await this.client.fetch({
+          service_name: this.serviceName,
+          account_id: accountId,
+          path: `/roles/${platformType}/${roleType}/latest`,
+      });
+      return role as ThemisRoleDocument;
   }
 
   async getRoles(accountId: string) {
-    const roles = await this.alClient.fetch({
-      service_name: this.serviceName,
-      account_id: accountId,
-      path: '/roles',
-    });
-    return roles as {roles: ThemisRoleDocument[]};
+      const roles = await this.client.fetch({
+          service_name: this.serviceName,
+          account_id: accountId,
+          path: '/roles',
+      });
+      return roles as { roles: ThemisRoleDocument[] };
   }
 
   async validateRoleCredentials(accountId: string, awsRole: AWSRole) {
-    const validate = await this.alClient.post({
-      service_name: this.serviceName,
-      account_id: accountId,
-      path: `/validate/${awsRole.platform_type}/${awsRole.role_type}`,
-    });
-    return validate as AWSRoleValidationResponse;
+      const validate = await this.client.post({
+          service_name: this.serviceName,
+          account_id: accountId,
+          path: `/validate/${awsRole.platform_type}/${awsRole.role_type}`,
+      });
+      return validate as AWSRoleValidationResponse;
   }
 }
 
-export const themisClient =  new ThemisClient();
+export const themisClient = new ThemisClient();
