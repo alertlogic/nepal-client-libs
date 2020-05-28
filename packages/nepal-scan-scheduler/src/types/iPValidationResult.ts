4969d10614f0bdc38a6f6ce8f02449023171d7ad
Jorge Mario Valencia <jmvalencia@users.noreply.github.com>
2020-05-28 13:22:20 -0500

    Fixing property names (#57)
    * Fixing property names
    
    * Updating version

--- packages/nepal-scan-scheduler/src/types/iPValidationResult.ts
+++ packages/nepal-scan-scheduler/src/types/iPValidationResult.ts
@@ -19,5 +19,5 @@ export interface AlIPValidationResult {
      * List of valid IP Addresses that can optionally be used as ScanScopeItems, but they are outside of deployment's scope of protection.
      * Scan targets with IP addresses that match `out_of_scope` items will not be scanned unless the scope of protection have been changed to include them.
      */
-    outOfScope?: (AlScanScopeItemCIDR | AlScanScopeItemIPRange | AlScanScopeItemIPAddress)[];
+    out_of_scope?: (AlScanScopeItemCIDR | AlScanScopeItemIPRange | AlScanScopeItemIPAddress)[];
 }
