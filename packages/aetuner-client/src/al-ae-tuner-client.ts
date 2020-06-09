4eaab9155ce68533f83ebf3c7fabc85f4a4c982f
Kyle Jensen <kjensen@alertlogic.com>
2020-06-09 11:02:25 -0500

    [ENG-4626] adding client lib for aetuner (#66)
    * aetuner initial
    
    * use my @al/core for this
    
    * add functions to aetuner
    
    * change test function
    
    * oops
    
    * oops
    
    * more typing
    
    * copy/paste fix
    
    * fix lint

---
+++ packages/aetuner-client/src/al-ae-tuner-client.ts
@@ -0,0 +1,178 @@
+/**
+ * Module to deal with available Analytic Engine's Tuner Public API endpoints
+ */
+import {
+    AlApiClient,
+    AlDefaultClient,
+} from '@al/core';
+import {
+    AnalyticHandlingUpdateObject,
+    AnalyticInfo,
+    AnalyticListReturn,
+    AnalyticUpdateObject,
+    HandlingSettings,
+    IncidentTypeInfo
+} from "./types/al-ae-tuner-client.type";
+
+export class AlAETunerClientInstance {
+
+    private serviceName = 'aetuner';
+
+    constructor(public client: AlApiClient = AlDefaultClient) {
+    }
+
+    /**
+     * List Analytics for a given account
+     * @param accountId
+     * @param datatype Data type used for analytics generation. If this parameter omitted, analytics for all data types are returned.
+     * @param output What to include in the response.
+     */
+    async getAnalytics(accountId: string, datatype?: string, output?: string):Promise<AnalyticListReturn> {
+        return this.client.get<AnalyticListReturn>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/analytics`,
+            params: {
+                datatype,
+                output
+            }
+        });
+    }
+
+    /**
+     * Returns an analytic for a specific customer
+     * @param accountId
+     * @param analytic Analytic Fully Qualified Name
+     */
+    async getAnalytic(accountId: string, analytic: string):Promise<AnalyticInfo> {
+        return this.client.get<AnalyticInfo>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/analytics/${analytic}`
+        });
+    }
+
+    /**
+     * Update an analytic
+     * @param accountId
+     * @param update
+     * @param analytic Analytic Fully Qualified Name
+     */
+    async updateAnalytic(accountId: string, update: AnalyticUpdateObject, analytic: string):Promise<unknown> {
+        return this.client.post<unknown>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/analytics/${analytic}`,
+            params: update
+        });
+    }
+
+    /**
+     * List all incident types for a specific customer
+     * @param accountId
+     */
+    async listIncidentTypes(accountId: string):Promise<string[]> {
+        return this.client.get<string[]>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_types`
+        });
+    }
+
+    /**
+     * Return incident type for a specific customer by it's name
+     * @param accountId
+     * @param incidentType Incident Type Fully Qualified Name
+     */
+    async getIncidentType(accountId: string, incidentType: string):Promise<IncidentTypeInfo> {
+        return this.client.get<IncidentTypeInfo>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_types/${incidentType}`
+        });
+    }
+
+    /**
+     *
+     * @param accountId
+     * @param update
+     * @param incidentType Incident Type Fully Qualified Name
+     */
+    async updateIncidentType(accountId, update: AnalyticUpdateObject, incidentType: string):Promise<unknown> {
+        return this.client.post<unknown>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_types/${incidentType}`,
+            params: update
+        });
+    }
+
+
+    /**
+     * Returns a list of all incident handling settings for a given customer
+     * @param accountId
+     */
+    async listIncidentHandling(accountId: string):Promise<HandlingSettings[]> {
+        return this.client.get<HandlingSettings[]>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_handling`
+        });
+    }
+
+    /**
+     * Return incident type handling setting
+     * @param accountId
+     * @param incidentType Incident Type Fully Qualified Name
+     */
+    async getIncidentHandling(accountId: string, incidentType: string):Promise<HandlingSettings> {
+        return this.client.get<HandlingSettings>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_handling/${incidentType}`
+        });
+    }
+
+    /**
+     * Set Incident Type handling setting
+     * @param accountId
+     * @param update
+     * @param incidentType Incident Type Fully Qualified Name
+     *
+     * @returns Object with a `stored` key and a UUID value.
+     */
+    async updateIncidentHandling(accountId: string, update: AnalyticHandlingUpdateObject, incidentType: string):Promise<{stored: string}> {
+        return this.client.post<{stored: string}>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_handling/${incidentType}`,
+            params: update
+        });
+    }
+
+    /**
+     * Delete Incident Handling setting
+     * @param accountId
+     * @param reason
+     * @param incidentType Incident Type Fully Qualified Name
+     */
+    async deleteIncidentHandling( accountId: string, reason: string, incidentType: string):Promise<{stored: string}> {
+        return this.client.post<{stored: string}>({
+            service_name: this.serviceName,
+            account_id: accountId,
+            version: 'v1',
+            path: `/incident_handling/${incidentType}`,
+            params: { reason }
+        });
+    }
+
+}
---
+++ packages/aetuner-client/src/index.ts
@@ -0,0 +1,8 @@
+
+import { AlGlobalizer } from '@al/core';
+import { AlAETunerClientInstance } from './al-ae-tuner-client';
+
+/* tslint:disable:variable-name */
+export const AlAETunerClient = AlGlobalizer.instantiate( "al.aetuner", () => new AlAETunerClientInstance() );
+
+export { AlAETunerClientInstance } from './al-ae-tuner-client';
