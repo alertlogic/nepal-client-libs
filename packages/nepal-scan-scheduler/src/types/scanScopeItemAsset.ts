fb77f73171d77fbf7250825819bc321be03daa16
Jorge Mario Valencia <vamajor@gmail.com>
2020-07-10 10:18:06 -0500

    Adding new types for scanScopeItemAsset

--- packages/nepal-scan-scheduler/src/types/scanScopeItemAsset.ts
+++ packages/nepal-scan-scheduler/src/types/scanScopeItemAsset.ts
@@ -10,20 +10,24 @@ export interface AlScanScopeItemAsset extends AlScanScopeItem {
      * - `vpc` (alias of the `network` type)
      * - `subnet`
      * - `host`
+     * - `external-ip`
+     * - `external-dns-name`
      */
     asset_type?: AlScanScopeItemAsset.AssetTypeEnum;
     key: string;
     type: AlScanScopeItemAsset.TypeEnum;
 }
 export namespace AlScanScopeItemAsset {
-    export type AssetTypeEnum = 'deployment' | 'region' | 'network' | 'vpc' | 'subnet' | 'host';
+    export type AssetTypeEnum = 'deployment' | 'region' | 'network' | 'vpc' | 'subnet' | 'host' | 'external-ip' | 'external-dns-name';
     export const assetTypeEnum = {
         Deployment: 'deployment' as AssetTypeEnum,
         Region: 'region' as AssetTypeEnum,
         Network: 'network' as AssetTypeEnum,
         Vpc: 'vpc' as AssetTypeEnum,
         Subnet: 'subnet' as AssetTypeEnum,
-        Host: 'host' as AssetTypeEnum
+        Host: 'host' as AssetTypeEnum,
+        ExternalIp: 'external-ip' as AssetTypeEnum,
+        ExternalDnsName: 'external-dns-name' as AssetTypeEnum
     };
     export type TypeEnum = 'asset';
     export const typeEnum = {
