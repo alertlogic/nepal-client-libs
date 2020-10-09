36acd3198ba4351deaca75d5e83cc10aecb6fd39
Andres Echeverri <andres.echeverri@alertlogic.com>
2020-10-08 22:21:22 -0500

    ENG-16938 - Generate Console protractor report in a HTML that can be imported in the PR

---
+++ packages/vr-test-helper/src/html-table.ts
@@ -0,0 +1,49 @@
+export class HTMLTable {
+    private table = '';
+    private hColumns: {key: string, value: string, width?: string }[] = [];
+    private header: string = '';
+    private body: string = '';
+
+    constructor(header: {key: string, value: string, width?: string}[], data: { [key: string]: string }[] = null){
+        this.buildHeader(header);
+        if(data && data.length > 0){
+            this.buildBody(data);
+        }
+    }
+
+    public buildHeader(header: {key: string, value: string, width?: string }[]): void {
+        this.hColumns = header;
+        this.header =
+            '<thead>'
+            + '<tr>'
+            + header.map(c => `<th scope="col" ${c.width ? 'style="width:' + c.width + ';"' : ''}> ${c.value} </th>`).join('')
+            + '</tr>'
+            + '</thead>';
+        this.buildTable();
+    }
+
+    public buildBody(data: { [key: string]: string }[]): void {
+        this.body =
+            '<tbody>'
+            + data.map(d => {
+                return '<tr>'
+                    + this.hColumns.map(c => {
+                        if (c.key in d && d[c.key]) {
+                            return `<td ${c.width ? 'style="width:' + c.width + ';"' : ''}> ${d[c.key]} </td>`;
+                        }
+                        return '<td></td>';
+                    }).join('')
+                    + '</tr>';
+            }).join('')
+            + '</tbody>';
+        this.buildTable();
+    }
+
+    public getTable(): string {
+        return this.table;
+    }
+
+    private buildTable() {
+        this.table = '<table class="table" style="table-layout: fixed;">'+this.header+this.body+'</table>';
+    }
+}
