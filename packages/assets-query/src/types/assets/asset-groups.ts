1d1d536f8de57e1d342210050d1b8ba2acdf30f0
Rob Parker <parky128@googlemail.com>
2020-10-16 15:29:57 +0100

    add asset group topology preview endpoint (#213)

--- packages/assets-query/src/types/assets/asset-groups.ts
+++ packages/assets-query/src/types/assets/asset-groups.ts
@@ -44,3 +44,54 @@ export interface AssetGroup {
     deleted_on: number;
     modified_on: number;
 }
+
+export interface AssetGroupTopologyResponse {
+    topology: AssetGroupTopology;
+}
+
+export interface AssetGroupTopology {
+    assets: string[][];
+    data: { [property: string]: TopologyAssetData };
+    rows: number;
+}
+
+// TODO - May be better to define an asset interface per asset type rather than merge all possible fields to a single interface
+// These fields should be enough for now for POC asset group work
+export interface TopologyAssetData {
+    account_id?: string;
+    alertlogic_appliance?: boolean;
+    architecture?: string;
+    availability_zone?: string;
+    cidr_block?: string;
+    created_on?: number;
+    deployment_id?: string;
+    dns_name?: string;
+    groups?: string[];
+    group_name?: string;
+    group_membership?: string;
+    group_id?: string;
+    instance_id?: string;
+    instance_name?: string;
+    instance_type?: string;
+    ip_address?: string;
+    key?: string;
+    launch_time?: number;
+    modified_on?: number;
+    name?: string;
+    native_type?: string;
+    private_dns_name?: string;
+    private_ip_address?: string;
+    private_ip_addresses?: string[];
+    private_ipv4_addresses?: string[];
+    private_ipv6_addresses?: string[];
+    public_ip_addresses?: string;
+    public_ipv4_addresses?: string[];
+    public_ipv6_addresses?: string[];
+    state?: string;
+    subnet_id?: string;
+    subnet_name?: string;
+    subnet_uuid?: string;
+    tags?: { [property: string]: string };
+    type?: string;
+
+}
