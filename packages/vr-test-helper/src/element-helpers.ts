9a19f753a8d3f63f3d150f821e14c621d3ee8a5b
Andres Echeverri <andres.echeverri@alertlogic.com>
2020-10-21 08:16:39 -0500

    ENG-16757 - Create the launcher script to run puppeteer test in paralell

--- packages/vr-test-helper/src/element-helpers.ts
+++ packages/vr-test-helper/src/element-helpers.ts
@@ -12,6 +12,7 @@ import {
 } from 'protractor';
 import { ConsoleMessageType, Page } from 'puppeteer';
 import { HTMLTable } from './html-table';
+import glob from "glob";
 
 export const WAIT_MAX_TIME = 35000;
 
@@ -20,7 +21,7 @@ interface Coordinates {
     y: number;
 }
 
-function ensureDirectoryExistence(filePath: string) {
+export function ensureDirectoryExistence(filePath: string) {
     const dirname = path.dirname(filePath);
     if (fs.existsSync(dirname)) {
         return;
@@ -54,7 +55,7 @@ export async function takeScreenshotPuppetter(page: Page, name: string) {
             return console.error(err);
         }
     });
-    await page.setViewport({ width: 1280, height: 766 });
+    await page.setViewport({ width: 1024, height: 768 });
     await page.screenshot({path: filename});
     return filename;
 }
@@ -179,30 +180,86 @@ export async function generateJSONConsoleReport(logs, png) {
  * @param types list of console methods to filter. (see ConsoleMessageType of Puppeteer)
  * @return void - generates a folder with the report and the evidence called console_error
  */
-export function consoleReportPuppeter(page: Page, types: ConsoleMessageType[] = []) {
+export function consoleJSONReport(page: Page, types: ConsoleMessageType[] = []) {
     page.on('console', async (msg) => {
         if (types === [] || types.includes(msg.type())) {
+            const reportName = './console_error/report-puppeteer.json';
             const title = await page.title();
-            const message = {
-                page: title ? title.replace(/\|/g, "-") : 'URL',
-                url: page.url(),
-                text: msg.text() ? msg.text().replace(/(\r\n|\n|\r)/gm, "<br>").replace(/\|/g, "-") : ''
-            };
             // Taking a screenshot when the error appears.
             try {
                 await page.waitFor(1000);
                 // saving report
                 const imgName = await takeScreenshotPuppetter(page, `./console_error/evidence/error`);
-                const row = `| ${message.text} | [${message.page}](${message.url}) | ![PR](${"%VISUAL_REGRESSION_URL%" + imgName.replace('./', '')}) |\n`;
-                fs.writeFile('./console_error/report_console_error.txt', row, { flag: 'a' }, (err) => {
-                    if (err) throw err;
-                    console.log('Adding information to the console report');
-                });
+                const data = {
+                    title: title ? title : 'URL',
+                    url: page.url(),
+                    message: msg.text() ? msg.text() : '',
+                    image: imgName.replace('./console_error/', '')
+                };
+                ensureDirectoryExistence(reportName);
+                if (!fs.existsSync(reportName)) {
+                    fs.writeFileSync(reportName, '{"data": [], "errors": 0, "specs": 0}');
+                }
+                let contents = fs.readFileSync(reportName, 'utf8');
+                let reportJson = JSON.parse(contents);
+                reportJson['data'].push(data);
+                reportJson['errors'] = parseInt(reportJson['errors'], 10) + 1;
+                try {
+                    fs.writeFileSync(reportName, JSON.stringify(reportJson));
+                } catch (err) {
+                    console.error("Error taking the screenshot", err);
+                }
             } catch (error) {
                 console.error("Error taking the screenshot", error);
             }
         }
     });
+}
+
+export function generateConsoleHTMLReportPuppeteer() {
+    const reportNameJson = './console_error/report-puppeteer.json';
+    const reportNameHTML = './console_error/report-puppeteer.html';
+
+    if (fs.existsSync(reportNameJson)) {
+        let contents = fs.readFileSync(reportNameJson, 'utf8');
+        let jsonData = JSON.parse(contents) as { data : {message: string, title:string, url: string, image: string}[], errors: number, specs: number };
+        let report = jsonData.data;
+        if(report && report.length > 0) {
+            let data = report.map(r => {
+                let message =
+                    '<ol>'
+                    + `<li><pre>${r.message.replace(/(\r\n|\n|\r)/gm, "<br>").replace(/\s/g, "&nbsp;")}</pre></li>`;
+                + '</ol>';
+                return {
+                    'col-1': message,
+                    'col-2': `<a href="${r.url}">${r.title}</a>`,
+                    'col-3': `<a href="${r.image}" target="_blank"><img class="img-fluid img-thumbnail" src="${r.image}" /></a>`
+                };
+            });
+            const styles = '<head>'
+                + '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">'
+                + `<style>
+                                pre {
+                                    white-space: pre-wrap;       /* css-3 */
+                                    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+                                    white-space: -pre-wrap;      /* Opera 4-6 */
+                                    white-space: -o-pre-wrap;    /* Opera 7 */
+                                    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+                                }
+                            </style>`
+                + '</head>';
+            let header = '<h1>Console Report</h1>'
+                + `<p><b>Total specs with errors in console: </b><span class="badge badge-pill badge-danger">${jsonData.specs}</span><br>`
+                + `<b>Total errors in console:</b><span class="badge badge-pill badge-danger">${jsonData.errors}</span></p>`;
+            const table = (new HTMLTable([
+                { key: 'col-1', value: 'Error', width: '50%' },
+                { key: 'col-2', value: 'Url', width: '25%' },
+                { key: 'col-3', value: 'Screenshot', width: '25%' }
+            ], data)).getTable();
+            const page = styles + header + table;
+            fs.writeFileSync(reportNameHTML, page);
+        }
+    }
 }
 
 export async function generateConsoleHTMLReport(logs, png) {
@@ -317,4 +374,69 @@ export async function generateVRHTMLReport() {
             fs.writeFileSync(reportNameHTML, page);
         }
     }
+}
+
+export async function e2eLogin(page: Page, email: string, password: string, waitForLogin: number = 11000) {
+    const emailSelector = 'input[name="email"]';
+    const passwordSelector = 'input[name="password"]';
+    try {
+        await page.waitForSelector(emailSelector, { timeout: waitForLogin });
+        await page.type(emailSelector, email);
+        await page.type(passwordSelector, password);
+        await page.click('.page-buttons button');
+        // wait to redirect
+        await page.waitFor(3000);
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
+
+export function generateScreenshotReportHTML() {
+    glob("./reports_e2e/screenshot-report-*.json", (er, files) => {
+        let style = `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                <style>
+                    ul {
+                        list-style: none;
+                    }
+                    li.passed:before {
+                        color:green;
+                        content: '✓ ';
+                    }
+                    li.failed:before {
+                        color:red;
+                        content: '✗ '
+                    }
+                    pre {
+                        font-size: 10px;
+                    }
+                </style>`;
+        let html = style;
+        for (let i = 0; i < files.length; i++) {
+            if (fs.existsSync(files[i])) {
+                let list = '<ul>';
+                let contents = fs.readFileSync(files[i], 'utf8');
+                let jsonData = JSON.parse(contents);
+                let specs = Object.keys(jsonData).sort();
+                for (let i = 0; i < specs.length; i++) {
+                    const spec = jsonData[specs[i]];
+                    list += `<li><strong>${spec.description}</strong>`;
+                    list += '<ul>';
+                    list += spec.specs.map(s => {
+                        let l = `<li class="${s.status}"><a href="${s.screenshot}" target="_blank">${s.description}</a>`;
+                        if ('failedExpectations' in s && s.failedExpectations.length > 0) {
+                            l += '<ul>';
+                            l += s.failedExpectations.map(f => `<li >${f.message}</li><li><pre>${f.stack}</pre></li>`).join('');
+                            l += '</ul>';
+                        }
+                        l += `</li>`;
+                        return l;
+                    }).join('');
+                    list += `</ul></li>`;
+                }
+                list += '</ul>';
+                html += list;
+            }
+        }
+        fs.writeFileSync('./reports_e2e/report.html', html);
+    });
 }
