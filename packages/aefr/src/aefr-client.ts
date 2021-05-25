e6378a8c0a74165e16a3d51a6345b961dba7782b
Kyle Jensen <kjensen@alertlogic.com>
2021-05-25 11:05:04 -0600

    aefr

---
+++ packages/aefr/src/aefr-client.ts
@@ -0,0 +1,28 @@
+import { AlApiClient, AlDefaultClient, AlLocation } from '@al/core';
+
+export class AefrClientInstance {
+    private serviceName = 'aefr';
+
+    public constructor(public client: AlApiClient = AlDefaultClient) {
+    }
+
+    /**
+     * Get Trigger
+     * GET
+     *
+     * @param accountId
+     * @param path string trigger path
+     * @param timestamp Epoch/Unix Timestamp
+     */
+    public async getByPath(accountId: string, path: string, timestamp: number) {
+        return this.client.get<any>({
+            account_id: accountId,
+            service_name: this.serviceName,
+            service_stack: AlLocation.GlobalAPI,
+            path: `/triggers/paths/${path}`,
+            version: 'v1',
+            params: { ts: timestamp }
+        });
+    }
+}
+
---
+++ packages/aefr/test/aefr-client.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import * as sinon from 'sinon';
+import { AefrClient } from '../src/index';
+
+afterEach(() => {
+    sinon.restore();
+});
+describe('Aefr Client Test Suite:', () => {
+    describe('when searching', () => {
+        let stub: sinon.SinonSpy;
+        beforeEach(() => {
+            stub = sinon.stub(AefrClient.client, 'get');
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+        it('should call get() on the AlDefaultClient instance', async () => {
+            await AefrClient.getByPath('fake', 'fake', 1234);
+            expect(stub.callCount).to.equal(1);
+        });
+    });
+
+});
