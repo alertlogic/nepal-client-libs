46642e86c0520653a5703ca59299f94881e4f7c7
Cristhian Fuertes <cristhian.e.fuertes@gmail.com>
2021-08-02 23:02:18 -0500

    ENG-29433: improve typedef for loki client (#461)

--- packages/loki/src/loki-client.type.ts
+++ packages/loki/src/loki-client.type.ts
@@ -1,2 +1,8 @@
 export type ListType = 'whitelist' | 'blacklist';
 export type IncidentType = string; // TODO: Type this out better.
+
+export interface IpAddressQueryResult {
+    contains: boolean;
+    ip_address: string;
+    list_key: string;
+}
