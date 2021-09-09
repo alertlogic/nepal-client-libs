a4a3229d8cccd6414d5e6c1d8206a818039ffd9f
BryanTabarez <BryanTabarez@users.noreply.github.com>
2021-09-09 16:33:00 -0500

    AlResponderTriggerBase: add role_id property (#481)

--- packages/responder/src/types/playbookTypes.ts
+++ packages/responder/src/types/playbookTypes.ts
@@ -514,6 +514,7 @@ interface AlResponderTriggerBase {
     enabled?: boolean;
     playbooks?: AlResponderTriggerPlaybookParameter[];
     actions?: AlResponderTriggerActionParameter[];
+    role_id?: string;
     playbooks_names?: string; // This is for UI only
 }
 
