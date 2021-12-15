c23e1d15d2146f9a4b1a9afbc692f860cf79a60a
Carlos Orozco <carlosorozco@alertlogic.com>
2021-12-15 11:50:45 -0500

    ENG-33803: Adding isExcluded endpoint into the exclusions client

--- packages/exclusions/src/al-exclusions-client.ts
+++ packages/exclusions/src/al-exclusions-client.ts
@@ -90,4 +90,20 @@ export class AlExclusionsClientInstance {
         return ExclusionsRulesDescriptor.import(rawData);
     }
 
+    /**
+     * @remarks https://console.product.dev.alertlogic.com/api/exclusions/#api-Is_Asset_Excluded_API-ApplyExclusionRule
+     */
+    async isExcluded(accountId: string, deploymentId: string, assetType: string, assetKey: string,
+                     feature: string = "scan", searchParams?: { [i: string]: string }): Promise<ExclusionsRulesDescriptor> {
+        const rawData = this.client.get({
+            service_stack: AlLocation.InsightAPI,
+            version: 'v2',
+            service_name: this.serviceName,
+            account_id: accountId,
+            path: `/${deploymentId}/is_excluded/${feature}/${assetType}/${assetKey}`,
+            params: searchParams
+        });
+        return ExclusionsRulesDescriptor.import(rawData);
+    }
+
 }
