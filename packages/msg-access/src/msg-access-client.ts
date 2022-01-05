4ac60e73b36f0737f3a0afc7a85af40ba612ef52
Jorge Mario Valencia <jmvalencia@alertlogic.com>
2022-01-05 17:08:32 -0500

    ENG-35212: First draft for msg_access API client

---
+++ packages/msg-access/src/msg-access-client.ts
@@ -0,0 +1,232 @@
+/**
+ * Module to deal with available Correlations Public API endpoints
+ */
+import {
+    AlDefaultClient
+} from '@al/core';
+
+
+export interface AlLogMessageMetaData {
+    account: number;
+    aid: number;
+    msgid: string;
+}
+
+export interface AlLogMessageFields {
+    time_recv: number;
+    hostname: string;
+    facility: string;
+    message: string;
+    parsed: {
+        rule_id: string;
+        rule_name: string;
+        pattern_id: string;
+        tokens: { [key: string]: string };
+    };
+    metadata: {
+        uuid: string;
+        create_ts: number;
+        dict: {
+            hostname: string;
+            public_hostname: string;
+            instance_id: string;
+            ip_addr: string;
+            public_ip_addr: string;
+            os: string;
+            os_version: string;
+            os_arch: string;
+            os_machine: string;
+        }
+    };
+    prioriy: number;
+    program: string;
+}
+
+export interface AlReadLogMessageResponse {
+    id: AlLogMessageMetaData;
+    fields: AlLogMessageFields | AlIdsMessageFields | AlFimDataFields | AlObservationFields;
+}
+
+export interface AlFlags {
+    urg: boolean;
+    syn: boolean;
+    rst: boolean;
+    psh: boolean;
+    ns: boolean;
+    fin: boolean;
+    ece: boolean;
+    cwr: boolean;
+    ack: boolean;
+    mf?: boolean;
+    df?: boolean;
+}
+
+export interface AlProtocol {
+    winsize: number;
+    urgent: number;
+    srcport: number;
+    seq: number;
+    options: string;
+    offset: number;
+    id: string;
+    flags: AlFlags;
+    dstport: number;
+    data_off: number;
+    checksum: number;
+    ack: number;
+    ttl?: number;
+    total_length?: number;
+    srcip: string;
+    ident?: number;
+    header_len?: number;
+    frag_offset?: number;
+    ecn?: number;
+    dstip: string;
+    diffserv?: number;
+    srcmac: string;
+    dstmac: string;
+}
+
+export interface AlProto {
+    srcport: number;
+    protocols: AlProtocol[];
+    protocol: number;
+    offset: number;
+    ip_src: string;
+    ip_dst: string;
+    dstport: number;
+}
+
+export interface AlPayload {
+    proto: AlProto;
+    data: string;
+    hex: string;
+    ascii: string;
+}
+
+export interface AlIdsMessageFields {
+    vlan?: number;
+    ts_us: number;
+    ts: number;
+    srcport: number;
+    signature: { [key: string]: unknown };
+    sig_rev: number;
+    sig_id: number;
+    sig_gen: number;
+    proto: number;
+    priority: number;
+    payload: AlPayload[];
+    orig?: boolean;
+    mpls?: number;
+    ip_src: string;
+    ip_proxy_client?: string;
+    ip_proxy?: string;
+    ip_dst: string;
+    ingest_ts: number;
+    ingest_id: string;
+    event_id: number;
+    dstport: number;
+    class: number;
+    asset_id: string;
+}
+
+export interface AlAsset {
+    dict: object;
+    data: string;
+    asset_id: string;
+}
+
+export interface AlFimDataFields {
+    ts: number;
+    sha1_hash: string;
+    path: string;
+    ingest_ts: number;
+    ingest_id: string;
+    host_id: string;
+    file_type: string;
+    file_size: number;
+    file_permissions: number;
+    file_owner: string;
+    file_name: string;
+    file_mtime: number;
+    file_group: string;
+    file_ctime: number;
+    file_attributes: number;
+    file_atime: number;
+    event_type: string;
+    asset: AlAsset;
+}
+
+export interface AlObservationFields {
+    visibility: string;
+    ts: number;
+    technique?: string;
+    tactic?: string;
+    summary: string;
+    subclass: string;
+    sub_technique?: string;
+    start_ts: number;
+    severity: string;
+    scope_type: string;
+    scope: string[];
+    recommendations: string;
+    properties_data: string;
+    properties: { [key: string]: any };
+    path: string;
+    parents: string[];
+    keys_data: string;
+    keys: { [key: string]: any };
+    internal_data: string;
+    internal: { [key: string]: any };
+    ingest_ts: number;
+    ingest_id: string;
+    id: string;
+    handling: string[];
+    evolved_to?: string;
+    end_ts: number;
+    desc: string;
+    confidence?: number;
+    class: string;
+    authority: string;
+}
+
+export class AlMsgAccessClientInstance {
+
+    private serviceName = 'msg_access';
+
+    /**
+     * Read Messages
+     * Read a set of messages from storage by ID. Proxy for daccess service messages API. Only addition is logmsgs data type messages are also parsed and tokenised
+     */
+    async readMessages(accountId: string, dataType: string = 'logmsgs', queryParams: { ids: string, fields?: string }): Promise<AlReadLogMessageResponse[]> {
+        // Let's set the fields default value
+        // which is to get all of them
+        if (!queryParams.fields) {
+            queryParams.fields = '__all';
+        }
+        return AlDefaultClient.get({
+            service_name: this.serviceName,
+            account_id: accountId,
+            path: `/messages/${dataType}`,
+            params: queryParams,
+        });
+    }
+
+    /**
+     * Read Messages POST
+     * Read a set of messages from storage by ID. Proxy for daccess service messages API. Only addition is logmsgs data type messages are also parsed and tokenised
+     */
+    async readMessagesPost(accountId: string, dataType: string = 'logmsgs', params: { ids: string[], fields?: string }): Promise<AlReadLogMessageResponse[]> {
+        // Let's set the fields default value
+        // which is to get all of them
+        if (!params.fields) {
+            params.fields = '__all';
+        }
+        return AlDefaultClient.post({
+            service_name: this.serviceName,
+            account_id: accountId,
+            path: `/messages/${dataType}`,
+            data: params
+        });
+    }
+}
---
+++ packages/msg-access/test/msg-access-client.spec.ts
@@ -0,0 +1,38 @@
+import { AlDefaultClient, AlLocation, AlLocatorService } from '@al/core';
+import {
+  assert,
+  expect,
+} from 'chai';
+import { describe } from 'mocha';
+import * as sinon from 'sinon';
+import { AlMsgAccessClient } from '../src/index';
+
+const serviceName = 'msg_access';
+
+beforeEach( () => {
+  AlDefaultClient.setGlobalParameters( { noEndpointsResolution: true } );
+} );
+afterEach(() => {
+  sinon.restore();
+});
+
+describe('msg_access Service Client Test Suite', () => {
+  const accountId = '2';
+  describe('when getting message details', () => {
+    let stub: sinon.SinonSpy;
+
+    beforeEach(() => {
+      stub = sinon.stub(AlDefaultClient as any, 'axiosRequest').returns(Promise.resolve({ status: 200, data: { }}));
+    });
+
+    afterEach(() => {
+      stub.restore();
+    });
+
+    it('should call the AlDefaultClient instance\'s GET.', async() => {
+      await AlMsgAccessClient.readMessages(accountId, undefined, { ids: '123' });
+      expect(stub.callCount).to.equal(1);
+      expect(stub.args[0][0].url).to.contains("/msg_access/v1/2/messages/logmsgs");
+    });
+  });
+});
