1e910aa55c888c49c1404843897ee6b69cd47a21
cristhian-fuertes <cristhian.e.fuertes@gmail.com>
2022-02-11 16:19:55 -0500

    ENG-36094: Updating types and adding endpoints

--- packages/convergence-utility/src/types/index.ts
+++ packages/convergence-utility/src/types/index.ts
@@ -1,54 +1,145 @@
 
 import { AlChangeStamp } from '@al/core';
 
-export type LookedUpUsersResponse = { users: {[id: string]: UserData }  };
+export type PolicyType = 'eventlogs' | 's3' | 'flatfile';
+
+export type LookedUpUsersResponse = { users: { [id: string]: UserData } };
 
 export type CollectionFiltersResponse = { filters: CollectionFilter[] };
 
 export type CollectionFilterValue = ICollectionFilterValue | string;
 
-export type CollectionSourceMetadata = {[i: string]: string};
+export type CollectionSourceMetadata = { [i: string]: string };
 
 export interface CloudExplorerAwsRegionRecord {
     id: string;
     name: string;
 }
 
-export interface CollectionResponse {
-    sources: CollectionSource[];
+export interface CollectionsGenericResponse {
+    credentials?: CollectionCredential;
+    policies?: CollectionPolicy[];
+    sources?: CollectionSource[];
+    collectors?: CollectionSource[];
+    hosts?: CollectionSource[];
     total_count?: number;
 }
 
+export interface CollectionCredential {
+    created?: AlChangeStamp;
+    modified?: AlChangeStamp;
+    id?: string;
+    name?: string;
+    type?: string;
+    password?: {
+        password: string,
+        username: string
+    };
+    iam_role?: {
+        arn: string,
+        external_id: string
+    };
+    azure_shared_key?: {
+        account_key: string,
+        account_name: string
+    };
+    azure_ad_user?: {
+        active_directory_id: string,
+        username: string
+    };
+}
+
+export interface CollectionPolicy {
+    id?: string;
+    name?: string;
+    type?: string;
+    product_type?: string;
+    collect_from_discovered?: boolean;
+    default?: boolean;
+    streams?: any[];
+    credential?: any;
+    schedule?: any;
+    created?: AlChangeStamp;
+    modified?: AlChangeStamp;
+    template_id: string;
+    s3: S3;
+    collection_alert?: CollectionAlert;
+    flatfile?: FlatFile;
+    alert_type?: string;
+    target_type?: string;
+}
+
+export interface FlatFile {
+    filename_integer_pattern?: string;
+    filename_timestamp_pattern?: string;
+    multiline?: MultilineConfig;
+    name?: string;
+    path?: string;
+    timestamp?: TimestampConfig;
+}
+
+export interface CollectionAlert {
+    alert_threshold?: number;
+    emails?: string[];
+    is_enabled?: boolean;
+    send_once?: boolean;
+    send_threshold?: number;
+}
+
+export interface S3 {
+    multiline?: MultilineConfig;
+    timestamp?: TimestampConfig;
+}
+
+export interface MultilineConfig {
+    continuation?: string;
+    is_multiline?: boolean;
+    is_regex?: boolean;
+    line_count?: string;
+    operator?: string;
+    pattern?: string;
+}
+
+export interface TimestampConfig {
+    format?: string;
+    rule?: string;
+}
+
 export interface CollectionSource {
-    source: CollectionSourceValue;
-    is_archived: boolean;
+    source?: CollectionSourceValue;
+    host?: CollectionSourceValue;
+    is_archived?: boolean;
 }
 
 export interface CollectionSourceValue {
+    network_id?: string;
+    registered?: boolean;
+    role?: boolean;
     collection_alerts?: string[];
     config?: CollectionSourceConfig;
     created?: AlChangeStamp;
     modified?: AlChangeStamp;
     customer?: { id: string };
     enabled?: boolean;
-    host?: {id: string, ip_address?: string};
+    host?: { id: string, ip_address?: string };
     appliance?: any;
     id?: string;
     name?: string;
     product_type?: string;
-    stats?: {[k: string]: number};
+    stats?: { [k: string]: number };
     status?: CollectionSourceStatus;
-    tags?: {name: string}[];
+    tags?: { name: string }[];
     type?: string;
     metadata?: CollectionSourceMetadata;
-    assignmentPolicy?: {id: string};
+    assignmentPolicy?: { id: string };
+    update_policy?: any;
 }
 
 export interface CollectionSourceStatus {
-    details: any[];
-    status: string;
-    timestamp: number;
-    updated: number;
+    details?: any[];
+    status?: string;
+    timestamp?: number;
+    updated?: number;
 }
 
 export interface CollectionSourceConfig {
