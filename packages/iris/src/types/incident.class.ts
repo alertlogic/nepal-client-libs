4f876ec8bfccb192d10c161598fa1c5b6c74054b
Darwin Garcia <dgarcia@alertlogic.com>
2022-05-31 10:34:27 -0500

    ENG-40092 - [UI] safeguard for 'related' property if not coming from API (#616)
    * ENG-40092 - [UI] skip looking for 'related' property from assets api call if API is not sending it
    
    * ENG-40092 - [UI] skip looking for 'related' property from assets api call if API is not sending it

--- packages/iris/src/types/incident.class.ts
+++ packages/iris/src/types/incident.class.ts
@@ -424,7 +424,9 @@ export class Incident {
 
                         // test this in production with
                         // /#/incidents/10785703/ea11106d32f14546/recommendations?aaid=10785703&locid=defender-us-denver
-                        if (i.assetDeploymentInScope) {
+
+                        // only try this if API is returning  the 'related' property for the host 
+                        if (i.assetDeploymentInScope && host.hasOwnProperty('related')) {
                             Object.entries(host.related)
                                   .filter(([key, value]) => ['auto-scaling-group', 'image', 'load-balancer', 'network-interface', 'sg', 'subnet'].includes(
                                       key))
