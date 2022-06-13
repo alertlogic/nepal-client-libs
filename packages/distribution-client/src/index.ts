f60c17cd061823ec3ca6968abcf4fdc24129ca62
cristhian-fuertes <cristhian.e.fuertes@gmail.com>
2022-06-13 17:41:55 -0500

    ENG-000: Removing uneccessary folders

--- packages/distribution-client/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { AlGlobalizer } from '@al/core';
-import { AlDistributorClientInstance } from './al-distributor-client';
-
-/* tslint:disable:variable-name */
-export const AlDistributorClient = AlGlobalizer.instantiate( "AlDistributorClient", () => new AlDistributorClientInstance() );
-/* tslint:enable:variable-name */
-
-export * from './al-distributor-client';
-
