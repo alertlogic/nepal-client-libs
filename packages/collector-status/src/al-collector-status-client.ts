1af17efaae075b4f4a131a3325e83f759f7842db
BryanTabarez <userbryan@gmail.com>
2022-06-28 17:13:58 -0500

    fix lint errors

--- packages/collector-status/src/al-collector-status-client.ts
+++ packages/collector-status/src/al-collector-status-client.ts
@@ -1,21 +1,16 @@
 /**
  * Collector status API client
  */
-import {
-    AlApiClient,
-    AlDefaultClient,
-    AlLocation
-} from '@al/core';
+import { AlApiClient, AlDefaultClient, AlLocation } from "@al/core";
 import {
     AlCollectorStatusHistoryResponse,
     AlCollectorStatusObject,
     AlCollectorStatusQueryParams,
-} from './types';
+} from "./types";
 
 export class AlCollectorStatusClientInstance {
-
     protected client: AlApiClient;
-    protected serviceVersion = 'v1';
+    protected serviceVersion = "v1";
     protected serviceName = "collectors_status";
 
     constructor(client: AlApiClient = null) {
@@ -28,13 +23,17 @@ export class AlCollectorStatusClientInstance {
      * /collectors_status/v1/{account_id}/statuses/{status_id}/history
      *
      * */
-    async getCollectorStatusHistory(accountId: string, statusId: string, params?: AlCollectorStatusQueryParams): Promise<AlCollectorStatusHistoryResponse> {
+    async getCollectorStatusHistory(
+        accountId: string,
+        statusId: string,
+        params?: AlCollectorStatusQueryParams
+    ): Promise<AlCollectorStatusHistoryResponse> {
         return this.client.get<AlCollectorStatusHistoryResponse>({
             service_stack: AlLocation.InsightAPI,
             service_name: this.serviceName,
             version: this.serviceVersion,
             account_id: accountId,
-            path: `/statuses/${ statusId }/history`,
+            path: `/statuses/${statusId}/history`,
             params: params,
         });
     }
@@ -46,13 +45,17 @@ export class AlCollectorStatusClientInstance {
      * /collectors_status/v1/{account_id}/statuses/{status_id}
      *
      * */
-     async getCollectorLastStatus(accountId: string, statusId: string, params?: AlCollectorStatusQueryParams): Promise<AlCollectorStatusHistoryResponse> {
+    async getCollectorLastStatus(
+        accountId: string,
+        statusId: string,
+        params?: AlCollectorStatusQueryParams
+    ): Promise<AlCollectorStatusHistoryResponse> {
         return this.client.get<AlCollectorStatusHistoryResponse>({
             service_stack: AlLocation.InsightAPI,
             service_name: this.serviceName,
             version: this.serviceVersion,
             account_id: accountId,
-            path: `/statuses/${ statusId }`,
+            path: `/statuses/${statusId}`,
             params: params,
         });
     }
@@ -64,15 +67,19 @@ export class AlCollectorStatusClientInstance {
      * /collectors_status/v1/{account_id}/statuses/{status_id}/streams/{stream}
      *
      * */
-     async reportCollectorStatus(accountId: string, statusId: string, stream: string, payload: AlCollectorStatusObject): Promise<void> {
+    async reportCollectorStatus(
+        accountId: string,
+        statusId: string,
+        stream: string,
+        payload: AlCollectorStatusObject
+    ): Promise<void> {
         return this.client.get<void>({
             service_stack: AlLocation.InsightAPI,
             service_name: this.serviceName,
             version: this.serviceVersion,
             account_id: accountId,
-            path: `/statuses/${ statusId }/streams/${ stream }`,
+            path: `/statuses/${statusId}/streams/${stream}`,
             data: payload,
         });
     }
-
 }
