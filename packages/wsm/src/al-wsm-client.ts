8814eb8e8bca0dd13b249e61e2c1cdba92661f31
Carlos Orozco <orozco1582@gmail.com>
2022-11-03 11:40:31 -0500

    ENG-44052: Base of WSM Api Client via YARD (#680)
    Co-authored-by: Carlos Orozco <carlosorozco@alertlogic.com>

---
+++ packages/wsm/src/al-wsm-client.ts
@@ -0,0 +1,59 @@
+/**
+ * Al WSM API client
+ */
+import {
+    AlDefaultClient
+} from '@al/core';
+import {
+    AlWSMAppliance,
+    AlWSMConfigAppliance
+} from './types';
+
+
+export class AlWSMClientInstance {
+
+    private targetEndpoint = 'yard';
+    private serviceName = 'api';
+
+    /**
+     * Get all appliances using an specific accountId
+     * GET
+     * /api/appliance/select?features=wsm_enabled&customer_ids={accountId}
+     * @param accountId AIMS Account ID
+     * @returns a promise with an array of appliances of a specific accountId
+     *
+     * @remarks
+     */
+    async getAppliances(accountId: string): Promise<AlWSMAppliance[]> {
+        return AlDefaultClient.get({
+            target_endpoint: this.targetEndpoint,
+            service_name: this.serviceName,
+            account_id: accountId,
+            path: `/appliance/select`,
+            params: {
+                features: 'wsm_enabled',
+                customer_ids: accountId
+            }
+        });
+    }
+
+    /**
+     * Get an appliance's full data using an specific applianceUuid
+     * GET
+     * /api/wsm/config/appliance/{applianceUuid}
+     *
+     * @param accountId AIMS Account ID
+     * @param applianceUuid Appliance UUID
+     * @returns a promise with the an Appliance object with all its properties
+     *
+     * @remarks
+     */
+    async getApplianceInfo(accountId: string, applianceUuid: string): Promise<AlWSMConfigAppliance> {
+        return AlDefaultClient.get({
+            target_endpoint: this.targetEndpoint,
+            service_name: this.serviceName,
+            account_id: accountId,
+            path: `/wsm/config/appliance/${applianceUuid}`
+        });
+    }
+}
