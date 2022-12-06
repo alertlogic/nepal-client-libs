4b21a9a647bd122e75c7167104294ec5eb07a094
Andres Echeverri <andres.echeverri@alertlogic.com>
2022-12-06 08:15:51 -0500

    fix unit test

--- packages/config/test/al-config-client-v2.spec.ts
+++ packages/config/test/al-config-client-v2.spec.ts
@@ -11,39 +11,37 @@ import {
 } from 'sinon';
 import { ALConfigV2 } from '../src/index';
 
-describe('CARGO CLIENT V2', () => {
-    let spy: SinonSpy;
-    const config = {
-        apiBaseURL: 'https://api.cloudinsight.alertlogic.com',
-        service: 'config',
-        version: 'v2',
-    };
-    const cargoURL = `${config.apiBaseURL}/${config.service}/${config.version}`;
-    const accountId = '2';
+describe('Config Client V2', () => {
+  let spy: SinonSpy;
+  const config = {
+    apiBaseURL: 'https://api.cloudinsight.alertlogic.com',
+    service: 'config',
+    version: 'v2',
+  };
+  const cargoURL = `${config.apiBaseURL}/${config.service}/${config.version}`;
+  const accountId = '2';
 
+  beforeEach(() => {
+    AlLocatorService.setContext({ environment: "production" });
+    return AlDefaultClient.setGlobalParameters({ noEndpointsResolution: true });
+  });
+
+  afterEach(() => restore());
+
+  describe('When list configs is called', () => {
     beforeEach(() => {
-      AlLocatorService.setContext( { environment: "production" } );
-      return AlDefaultClient.setGlobalParameters({ noEndpointsResolution: true });
+      spy = stub(AlDefaultClient as any, 'axiosRequest').returns(Promise.resolve({ status: 204 }));
     });
-    afterEach(() => restore());
 
-    describe('Schedules', () => {
-        describe('When batch delete is called', () => {
-            beforeEach(() => {
-                spy = stub(AlDefaultClient as any, 'axiosRequest').returns(Promise.resolve({status: 204}));
-            });
-
-            it('Should call the batchDeleteSchedules (DELETE).', async () => {
-                const path = `configs`;
-                await ALConfigV2.listConfigs(accountId, {'asset_type': 'host'});
-                expect(spy.callCount).to.equal(1);
-                expect(spy.getCall(0).args[0].version).to.equal(config.version);
-                expect(spy.getCall(0).args[0].method).to.equal('GET');
-                expect(spy.getCall(0).args[0].account_id).to.equal(accountId);
-                expect(spy.getCall(0).args[0].path).to.equal(`/${path}`);
-                expect(spy.getCall(0).args[0].url).to.equal(`${cargoURL}/${accountId}/${path}`);
-            });
-
-        });
+    it('Should call listConfigs function', async () => {
+      const path = `configs`;
+      await ALConfigV2.listConfigs(accountId, { 'asset_type': 'host' });
+      expect(spy.callCount).to.equal(1);
+      expect(spy.getCall(0).args[0].version).to.equal(config.version);
+      expect(spy.getCall(0).args[0].method).to.equal('GET');
+      expect(spy.getCall(0).args[0].account_id).to.equal(accountId);
+      expect(spy.getCall(0).args[0].path).to.equal(`/${path}`);
+      expect(spy.getCall(0).args[0].url).to.equal(`${cargoURL}/${accountId}/${path}`);
     });
+  });
 });
