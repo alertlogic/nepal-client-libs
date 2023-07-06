aa8bc4155e3e742e21ff72d91b65dba2fb457d73
Jhonatannr <jhonatanoguera7019@gmail.com>
2023-07-06 15:25:45 -0500

    ENG-49531: Tuning client endpoints and test fix

--- packages/aecontent/src/aecontent-client.ts
+++ packages/aecontent/src/aecontent-client.ts
@@ -1,4 +1,5 @@
 import { AlApiClient, AlDefaultClient, AlLocation } from '@al/core';
+import { AlTuningRule } from './types/aecontent-client-tuning.type';
 
 export class AecontentClientInstance {
     private serviceName = 'aecontent';
@@ -19,6 +20,60 @@ export class AecontentClientInstance {
             path: `/observations/paths/${path}`,
             version: 'v1',
             params: { ts: timestamp }
+        });
+    }
+
+    /**
+     * create Tuning rule
+     * POST
+     *
+     * @param accountId string tuning rule's belonging account id
+     *
+     */
+    public async createRule(accountId: string, data: AlTuningRule): Promise<any> {
+        return this.client.post<any>({
+            data,
+            service_name: this.serviceName,
+            path: `${accountId}/tunings`,
+            version: 'v1'
+        });
+    }
+
+    /**
+     * delete Tuning rule
+     * DELETE
+     *
+     * @param accountId string tuning rule's belonging account id
+     * @param name string tuning rule's name
+     *
+     */
+    public async deleteRule(accountId: string, name: string): Promise<any> {
+        return this.client.get<any>({
+            service_name: this.serviceName,
+            path: `${accountId}/tunings`,
+            version: 'v1',
+            params: {
+                        ruleName: name
+                    }
+        });
+    }
+
+    /**
+     * Get Tuning rule
+     * GET
+     *
+     * @param accountId string tuning rule's belonging account id
+     * @param name string tuning rule's name
+     *
+     */
+    public async getRule(accountId: string, name: string): Promise<any> {
+        return this.client.get<any>({
+            service_name: this.serviceName,
+            path: `${accountId}/tunings`,
+            version: 'v1',
+            params: {
+                        ruleName: name
+                    }
         });
     }
 }
