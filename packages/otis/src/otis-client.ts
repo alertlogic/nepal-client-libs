121e68897c68afaf31272b9e8ad65aa6fb4ff3a9
Cristhian Fuertes <cristhian.e.fuertes@gmail.com>
2023-07-14 14:38:11 -0500

    ENG-41929: Updating Otis types (#745)

--- packages/otis/src/otis-client.ts
+++ packages/otis/src/otis-client.ts
@@ -7,6 +7,9 @@ export interface TuningOptionScope {
   deployment_id?: string;
   region_key?: string;
   vpc_key?: string;
+  vpc_id?: string;
+  provider_type?: string;
+  provider_id?: string;
 }
 
 export type TuningOptionValue = string | number | boolean | {[key:string]: unknown};
