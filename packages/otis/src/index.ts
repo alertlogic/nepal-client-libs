131b21264f6a11aca2786c759683d0de5e4e8bd9
cristhian-fuertes <cristhian.e.fuertes@gmail.com>
2023-07-18 17:29:27 -0500

    ENG-41929: splitting Otis into 2 clients

--- packages/otis/src/index.ts
+++ packages/otis/src/index.ts
@@ -1,6 +1,10 @@
 export {
-    otisClient as OTISClient,
-    TuningOption,
-    TuningOptionScope,
-    TuningOptionValue
-} from './otis-client';
+    otisClient as OTISClient
+} from './otis-client.v3';
+
+export {
+    otisClientV2 as OtisClientV2
+} from './otis-client.v2';
+
+export * from './types';
+
