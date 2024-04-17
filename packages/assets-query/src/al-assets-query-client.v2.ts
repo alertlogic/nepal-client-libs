f2256749b85f3cbb92258ed44735938fc60bfb95
FairAndresAL <61560022+FairAndresAL@users.noreply.github.com>
2024-04-17 13:03:26 -0500

    ENG-54985 - Adding Filters to Exposure-filters API (#814)
    Co-authored-by: Fair Tarapues <fairtarapues@AL-COL-15974.local>

--- packages/assets-query/src/al-assets-query-client.v2.ts
+++ packages/assets-query/src/al-assets-query-client.v2.ts
@@ -6,7 +6,7 @@ import {
   AlDefaultClient,
   AlLocation
 } from '@al/core';
-import { ExposureFilter } from './types';
+import { ExposureFilter, ExposureQueryParams } from './types';
 
 
 export class AlAssetsQueryV2ClientInstance {
@@ -25,13 +25,14 @@ export class AlAssetsQueryV2ClientInstance {
    * @param accountId - The account id for which exposure filters are to be retrieved.
    * @returns A Promise that resolves to an array of ExposureFilter objects.
    */
-  async getExposureFilters(accountId: string): Promise<{filters: ExposureFilter[]}> {
+  async getExposureFilters(accountId: string,queryParams?: ExposureQueryParams): Promise<{filters: ExposureFilter[]}> {
     return await this.client.get({
       service_stack: AlLocation.InsightAPI,
       version: this.version,
       account_id: accountId,
       service_name: 'assets_query',
-      path: `/exposure-filters`
+      path: `/exposure-filters`,
+      params: queryParams,
     });
   }
 }
